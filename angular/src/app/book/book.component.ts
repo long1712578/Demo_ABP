5e1c815c1f7342eac2a459d8ef50067fa35e2519
Long1712578 <pdlong578@hmail.com>
2021-01-19 17:45:21 +0700

    CRUD

---
+++ angular/src/app/book/book.component.ts
@@ -0,0 +1,79 @@
+import { ListService, PagedResultDto } from '@abp/ng.core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { BookDto } from '@proxy';
+import { BookService, bookTypeOptions } from '@proxy/books';
+import { NgbDateNativeAdapter, NgbDateAdapter } from '@ng-bootstrap/ng-bootstrap';
+import { Confirmation, ConfirmationService } from '@abp/ng.theme.shared';
+
+
+@Component({
+  selector: 'app-book',
+  templateUrl: './book.component.html',
+  styleUrls: ['./book.component.scss'],
+  providers: [ListService, { provide: NgbDateAdapter, useClass: NgbDateNativeAdapter }],
+})
+export class BookComponent implements OnInit {
+
+  book = { items: [], totalCount: 0 } as PagedResultDto<BookDto>;
+  isModalOpen = false; // add this line
+  form: FormGroup;
+  bookTypes = bookTypeOptions;
+  selectedBook = {} as BookDto; // declare selectedBook
+
+  constructor(public readonly list: ListService, private bookService: BookService, private fb: FormBuilder, private confirmation: ConfirmationService) { }
+
+  ngOnInit() {
+    const bookStreamCreator = (query) => this.bookService.getList(query);
+
+    this.list.hookToQuery(bookStreamCreator).subscribe((response) => {
+      this.book = response;
+    });
+  }
+  // add new method
+  createBook() {
+    this.buildForm();
+    this.isModalOpen = true;
+  }
+  // Add editBook method
+  editBook(id: string) {
+    this.bookService.get(id).subscribe((book) => {
+      this.selectedBook = book;
+      this.buildForm();
+      this.isModalOpen = true;
+    });
+  }
+  // Add a delete method
+  delete(id: string) {
+    this.confirmation.warn('::AreYouSureToDelete', '::AreYouSure').subscribe((status) => {
+      if (status === Confirmation.Status.confirm) {
+        this.bookService.delete(id).subscribe(() => this.list.get());
+      }
+    });
+  }
+  //add buildform metho
+  buildForm() {
+    this.form = this.fb.group({
+      name: ['', Validators.required],
+      type: [null, Validators.required],
+      publishDate: [null, Validators.required],
+      price: [null, Validators.required],
+    })
+  }
+  // add save method
+  save() {
+    if (this.form.invalid) {
+      return;
+    }
+
+    const request = this.selectedBook.id
+      ? this.bookService.update(this.selectedBook.id, this.form.value)
+      : this.bookService.create(this.form.value);
+
+    request.subscribe(() => {
+      this.isModalOpen = false;
+      this.form.reset();
+      this.list.get();
+    });
+  }
+}
